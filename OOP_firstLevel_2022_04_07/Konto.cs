6e404b06430efd7170406c97a8993c6cc19e2eac
Stefan.Albert <Stefan.Albert@BFW-H6-PC10>
2022-04-08 10:26:44 +0200

    Projektdateien hinzufügen.

---
+++ OOP_firstLevel_2022_04_07/Konto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_firstLevel_2022_04_07
+{
+    class Konto
+    {
+        // private Attribute anlegen
+        private int kontoNr;
+        private string besitzer;
+        protected double kontostand;
+
+        //Konstruktor mit Werten belegen
+        public Konto()
+        {
+            this.kontoNr = 1;
+            this.besitzer = "IT3";
+            this.kontostand = 0.0;
+        }
+
+        //Schnittstellen nach außen der Klasse 
+        //definieren get- bzw. set-Methoden
+        public int KontoNr
+        {
+            get { return this.kontoNr; }
+            set { this.kontoNr = value; }
+        }
+
+        public string Besitzer
+        {
+            get { return this.besitzer; }
+            set { this.besitzer = value; }
+        }
+
+        public double Kontostand
+        {
+            get { return this.kontostand; }
+            set { this.kontostand = value; }
+        }
+
+    }
+}
---
+++ OOP_firstLevel_2022_04_07/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOP_firstLevel_2022_04_07
+{
+    class Program
+    {
+
+        //ein Objekt einer Instanz der Klasse erzeugen
+        static Konto konto = null;
+        static void Main(string[] args)
+        {
+            Console.Title = "Kontoverwaltung";
+
+        }
+        static void KontoAnlegen()
+        {
+            konto = new Konto(); //konto erzeugen und mit Daten füllen
+        }
+        // Methode für Menü anzeigen
+        static void MenuAnzeige ()
+        {
+            Console.Clear(); // löscht Bildschirmanzeige
+            Console.WriteLine("Bitte Aktion auswählen");
+            Console.WriteLine("1. Kontodaten anzeigen");
+            Console.WriteLine("2. Geld einzahlen");
+            Console.WriteLine("4. Prgramm beenden");
+            Console.WriteLine();
+            //Aufforderung zur Eingabe
+            Console.WriteLine("Bitte passende Zahl eingeben: ");
+        }
+    }
+}
